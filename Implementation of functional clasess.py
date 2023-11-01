e4edd5cd6222a1efd300c6d9f95b53c44f270a03
fort_nassa <curashi91@gmail.com>
2023-11-01 23:23:51 +0100

    added class instances

--- Implementation of functional clasess.py
+++ Implementation of functional clasess.py
@@ -10,8 +10,8 @@ class Student:
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
 
-    def rate_for_course(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.grades:
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if grade in lecturer.grades:
                 lecturer.grades[course] += [grade] #реализация закрепленного кура за Лекторами, для того что бы осуществить проверку оценок
             else:
@@ -19,15 +19,39 @@ class Student:
         else:
             return "Ошибка"
 
+    def average_rating(self):
+        total_rating = 0
+        total_count = 0
+
+        for grades in self.grades.values():
+            total_rating += sum(grades)
+            total_count += len(grades)
+
+            if total_rating == 0:
+                return 0.0
+            else:
+                return total_rating/total_count
+
+    def __eq__(self, other):
+        return self.average_rating() == other.avarage_rating()
+
+    def __ne__(self, other):
+        return self.average_rating() != other.avarage_rating()
+
+    def __lt__(self, other):
+        return self.average_rating() < other.avarage_rating()
+
+    def __le__(self, other):
+        return self.average_rating() <= other.avarage_rating()
+
     def __str__(self):
-        f'Имя: {self.name}\n Фамилия: {self.surname}\n Средняя оценка за домшнее задание: {self.grades}\n Курсы в процессе изучения: {self.courses_in_progress}\n Завершенные курсы: {self.finished_courses}'
+        return f'Имя: {self.name}\n Фамилия: {self.surname}\n Средняя оценка за домшнее задание: {self.grades}\n Курсы в процессе изучения: {self.courses_in_progress}\n Завершенные курсы: {self.finished_courses}'
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
-
 
 
 class Lecturer(Mentor):
@@ -41,12 +65,11 @@ class Lecturer(Mentor):
             self.grades[course] = []
         self.grades[course].append(grade)
 
-
-    def avarage_rating(self, grades):
+    def average_rating(self):
         #нужно пройтись по списку используя for или list comprehension
         total_rating = 0
         total_count = 0
-        
+
         for grades in self.grades.values():
             total_rating += sum(grades)
             total_count += len(grades)
@@ -55,9 +78,20 @@ class Lecturer(Mentor):
         else:
             return total_rating/total_count
 
+    def __eq__(self, other):
+        return self.average_rating() == other.avarage_rating()
+
+    def __ne__(self, other):
+        return self.average_rating() != other.avarage_rating()
+
+    def __lt__(self, other):
+        return self.average_rating() < other.avarage_rating()
+
+    def __le__(self, other):
+        return self.average_rating() <= other.avarage_rating()
+
     def __str__(self):
         return f'Имя: {self.name}\n Фамилия: {self.surname}\n Средняя оценка за лекции: {self.courses_attached}'
-
 
 
 class Reviewer(Mentor):
@@ -72,10 +106,24 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return "Ошибка"
+
     def __str__(self):
         return f'Имя: {self.name}\n Фамилия: {self.surname}'
 
 
+# def avg_courses(students_name, course_name):
+#     if
+student1 = Student('Chandler', 'Bings', 'man')
+student1.finished_courses += ['Основы языка программирования Python']
+student1.courses_in_progress += ['ООП и работа с API']
+student1.grades['Основы языка программирования Python'] = [9, 10, 9, 8]
+student1.grades['ООП и работа с API'] = [8, 10]
+
+print(student1.name)
+print(student1.gender)
+print(student1.finished_courses)
+print(student1.courses_in_progress)
+print(student1.grades)
 # best_student = Student('Ruoy', 'Eman', 'your_gender')
 # best_student.finished_courses += ['Git']
 # best_student.courses_in_progress += ['Python']
