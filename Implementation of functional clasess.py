9f4ce23832429b27f8e6d02ed58ef44cbba42c6b
fort_nassa <curashi91@gmail.com>
2023-11-06 06:51:23 +0100

    chang 05.11

--- Implementation of functional clasess.py
+++ Implementation of functional clasess.py
@@ -13,7 +13,7 @@ class Student:
     def rate_lecturer(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if grade in lecturer.grades:
-                lecturer.grades[course] += [grade] #реализация закрепленного кура за Лекторами, для того что бы осуществить проверку оценок
+                lecturer.grades[course] += [grade]
             else:
                 lecturer.grades[course] = [grade]
         else:
@@ -66,7 +66,6 @@ class Lecturer(Mentor):
         self.grades[course].append(grade)
 
     def average_rating(self):
-        #нужно пройтись по списку используя for или list comprehension
         total_rating = 0
         total_count = 0
 
@@ -96,7 +95,7 @@ class Lecturer(Mentor):
 
 class Reviewer(Mentor):
     def __init__(self, name, surname, courses_attached):
-        super().__init__(name, surname, courses_attached)
+        super().__init__(name, surname)
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.course_in_progress:
@@ -111,43 +110,113 @@ class Reviewer(Mentor):
         return f'Имя: {self.name}\n Фамилия: {self.surname}'
 
 
-# def avg_courses(students_name, course_name):
-#     if
+
 student1 = Student('Chandler', 'Bings', 'man')
 student1.finished_courses += ['Основы языка программирования Python']
 student1.courses_in_progress += ['ООП и работа с API']
 student1.grades['Основы языка программирования Python'] = [9, 10, 9, 8]
 student1.grades['ООП и работа с API'] = [8, 10]
 
+student2 = Student('Monica', 'Bing', 'woman')
+student2.finished_courses += ['ООП и работа с API']
+student2.courses_in_progress += ['Git']
+student2.grades['ООП и работа с API'] = [10, 10, 10, 9]
+student2.grades['Git'] = [10, 10]
+
+
 print(student1.name)
+print(student2.name)
 print(student1.gender)
+print(student2.gender)
 print(student1.finished_courses)
+print(student2.finished_courses)
 print(student1.courses_in_progress)
+print(student2.courses_in_progress)
 print(student1.grades)
-# best_student = Student('Ruoy', 'Eman', 'your_gender')
-# best_student.finished_courses += ['Git']
-# best_student.courses_in_progress += ['Python']
-# best_student.grades['Git'] = [10, 10, 10, 10, 10]
-# best_student.grades['Python'] = [10, 10]
-#
-# print(best_student.finished_courses)
-# print(best_student.courses_in_progress)
-# print(best_student.grades)
-#
-# cool_mentor = Mentor('Some', 'Buddy')
-# cool_mentor.courses_attached += ['Python']
-# print(cool_mentor.courses_attached)
-#
-# best_student = Student('Ruoy', 'Eman', 'your_gender')
-# best_student.courses_in_progress += ['Python']
-#
-# cool_mentor = Mentor('Some', 'Buddy')
-# cool_mentor.courses_attached += ['Python']
-#
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-#
-# print(best_student.grades)
+print(student2.grades)
+print(student1.average_rating())
+print(student2.average_rating())
+
+if student1 > student2:
+    print(f'Студент {student1} учится лучше, чем {student2}')
+else:
+    print(f'Студент {student2} учится лучше, чем {student1}')
+
+mentor1 = Mentor('Sheldon' 'Cooper')
+mentor1.courses_attached += []
+
+mentor2 = Mentor('Thomas', 'Shelby')
+mentor2.courses_attached += []
+
+print(mentor1.name)
+print(mentor2.name)
+print(mentor1.surname)
+print(mentor2.surname)
+print(mentor1.courses_attached)
+print(mentor2.courses_attached)
+
+lecturer1 = Lecturer('Ted', 'Mosbey')
+lecturer1.courses_attached += []
+lecturer1.grades += {}
+
+lecturer2 = Lecturer ('Bruce', 'Banner')
+lecturer2.courses_attached += []
+lecturer2.grades += {}
+
+print(lecturer1.name)
+print(lecturer2.name)
+print(lecturer1.surname)
+print(lecturer2.surname)
+print(lecturer1.courses_attached)
+print(lecturer2.courses_attached)
+print(lecturer1.grades)
+print(lecturer2.grades)
+print(lecturer1.average_rating())
+print(lecturer2.average_rating())
+
+reviewer1 = Reviewer('Cameron', 'Diaz')
+reviewer1.rate_hw(student2)
+
+reviewer2 = Reviewer('Solomon', 'Grandy')
+reviewer2.rate_hw(student1)
+
+print(reviewer1.name)
+print(reviewer2.name)
+print(reviewer1.surname)
+print(reviewer2.surname)
+print(reviewer1.rate_hw(student1))
+print(reviewer2.rate_hw(student2))
+
+def all_students_rate(student_list, course_name):
+    some_list = []
+    sum_ = 0
+    count = 0
+    for student_hw in students_list:
+        for k,v in rate_hw.grades.items():
+            if k == course_name:
+                some_list.append(value)
+        for i in some_list:
+            for n in i:
+                sum_ += n
+                count += 1
+        avg_rating_all = sum_/ count
+        return avg_rating_all
+
+def all_lecturer_rate(lecturer_list, course_name):
+    some_list = []
+    sum_ = 0
+    count = 0
+
+    for lecturer_lection in lecturer_list:
+        for k,v in rate_lecturer:
+            if k == course_name:
+                some_list.append(value):
+                for i in some_list:
+                    for n in i:
+                        sum_ += n
+                        count += 1
+                        avg_rating_all_lecturer = sum_/count
+                return avg_rating_all_lecturer
+
 
 
